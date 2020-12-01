8b7a736e5ae4c580656f125e06888bce2b06de36
DaoGod <epistre31570@gmail.com>
2020-12-01 12:32:39 +0100

    [ADD] commit de toute la première scéance

--- app/src/main/java/com/ynov/todolist/MainActivity.java
+++ app/src/main/java/com/ynov/todolist/MainActivity.java
@@ -12,8 +12,16 @@ import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,16 +29,32 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        listView = (ListView)findViewById(R.id.listView);
+       /* ArrayAdapter<String> tableau =
+                new ArrayAdapter<String>(listView.getContext(),
+                        R.layout.text_field,
+                        R.id.textView);
+        for (int i=0; i<40; i++) {
+            tableau.add("coucou " + i); }
+        list.setAdapter(tableau);*/
+        final ArrayList<String> arrayList= new ArrayList<>();
+        arrayList.add("android");
+        arrayList.add("android");
+        arrayList.add("is");
+        arrayList.add("genious");
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, arrayList);
+        listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(MainActivity.this, "clicked item: " +i+ " " +arrayList.get(i).toString(), Toast.LENGTH_SHORT).show();
             }
         });
+
+
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
--- app/src/main/java/com/ynov/todolist/SecondActivity.java
+++ app/src/main/java/com/ynov/todolist/SecondActivity.java
@@ -1,4 +1,24 @@
 package com.ynov.todolist;
 
-public class SecondActivity {
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity  extends AppCompatActivity {
+
+    EditText editText;
+    Button button1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_activity);
+        button1 = findViewById(R.id.button1);
+        editText = (EditText)findViewById(R.id.editText);
+        
+
+
+    }
 }
